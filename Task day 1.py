6f8457e6233c87b2dd32da77f888aff5fd95109f
rishathrishi69-prog <rishathrishi69@gmail.com>
2026-08-11 20:19:15 +0530

    Add files via upload

---
+++ Task day 1.py
@@ -0,0 +1,8 @@
+#This is my first day in python
+#This is my python code 
+print("Welcome Rishath")
+name=input("Enter your name")
+name=input("Enter your department")
+name=input("Enter your phone number")
+name=input("which is your favourite subject")
+name=input("what is your Aim")
